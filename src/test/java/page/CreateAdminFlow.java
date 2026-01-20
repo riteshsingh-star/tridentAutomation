34f71272fc87970fb1108b31e662682a02a235f1
Vinayakkkk <vinayakhanagi1234@gmail.com>
2026-01-20 18:04:14 +0530

    Second code

--- src/test/java/page/CreateAdminFlow.java
+++ src/test/java/page/CreateAdminFlow.java
@@ -1,52 +1,76 @@
 package page;
 
+import base.BasePage;
 import com.microsoft.playwright.BrowserContext;
+import com.microsoft.playwright.Locator;
 import com.microsoft.playwright.Page;
 import com.microsoft.playwright.options.AriaRole;
-import com.trident.playwright.Base.BasePage;
+
 
 public class CreateAdminFlow extends BasePage {
 
-    Page page;
+
     BrowserContext context;
     Page childPage;
 
     public CreateAdminFlow(Page page, BrowserContext context) {
-        this.page = page;
+        super(page);
+
         this.context = context;
         this.childPage = OpenAdminPage.moveToAdminPage(page, context);
     }
 
-    public void createGlobalParameter() throws InterruptedException {
+//    public void createGlobalParameter() throws InterruptedException {
+//
+//        childPage.click("li[id='Global Parameters List']");
+//        syncUntil(2000);
+//        childPage.click("//button[@id='createNewGlobalParameter']");
+//        syncUntil(1000);
+//        childPage.fill("//input[@id='parameterName']","NewBatchBB");
+//        syncUntil(1000);
+//        childPage.click("//*[@class=' css-1hwfws3']");
+//        syncUntil(1000);
+//        childPage.click("//*[contains(text(),'Double')]");
+//        syncUntil(1000);
+//        childPage.check("input[value='batch']");
+//        syncUntil(1000);
+//        childPage.click("//span[text()='Save']//parent::button");
+//    }
+//
+    public void dragNewBatchBatchToDefinition() {
 
-        childPage.click("li[id='Global Parameters List']");
-        syncUntil(2000);
-        childPage.click("//button[@id='createNewGlobalParameter']");
-        syncUntil(1000);
-        childPage.fill("//input[@id='parameterName']","New_Batch");
-        syncUntil(1000);
-        childPage.click(".css-x1u5dn-control");
-        childPage.click("text=Integer");
-        syncUntil(1000);
-        childPage.check("input[value='batch']");
-        syncUntil(1000);
-        childPage.click(".jss242");
+        Locator source = childPage.locator("text=NewBatchBB");
+
+        Locator target = childPage.getByRole(AriaRole.TEXTBOX);
+
+        source.dragTo(target);
     }
 
     public  void createNewKPIDefinition() throws InterruptedException{
 
         childPage.click("li[id='New KPI Definition']");
         syncUntil(2000);
-        childPage.check(".jss134 jss128 jss568 jss572 jss576");
+        childPage.fill("//input[@id='kpiName']","BH");
+        childPage.selectOption("#selectedPlant","covacsis_dev");
         childPage.getByRole(AriaRole.CHECKBOX,
                         new Page.GetByRoleOptions().setName("Aluminium"))
                 .check();
-        childPage.check(".jss571");
-        childPage.click("(//button[@type='button'])[7]");
+        childPage.check("input[value='batch']");
+        childPage.click("body > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(3) > button:nth-child(1)");
+        syncUntil(2000);
+        childPage.locator("(//li[@class='jss18245 active'])[1]").click();
+        dragNewBatchBatchToDefinition();
+        syncUntil(2000);
+        childPage.selectOption("body > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(3) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(6) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > select:nth-child(1)","AVERAGE");
+        syncUntil(2000);
+        childPage.selectOption("(//select[@id='fromUnit'])[2]","More Value is Good");
+        syncUntil(2000);
+        childPage.selectOption("(//select[@id='fromUnit'])[3]","HOUR");
+        childPage.fill("//input[@id='precision']","2");
+        childPage.click("(//button[@type='button'])[8]");
+   }
 
 
-
-    }
 
     public  void addLogicToTheKPIAndValidate(){
 
--- src/test/java/test/AdminFlowTest.java
+++ src/test/java/test/AdminFlowTest.java
@@ -1,8 +1,9 @@
-package Test;
+package test;
 
-import Page.CreateAdminFlow;
-import Page.LoginPage;
-import com.trident.playwright.Base.BaseTest;
+import base.BaseTest;
+import page.CreateAdminFlow;
+import page.LoginPage;
+
 import org.testng.annotations.Test;
 
 public class AdminFlowTest extends BaseTest {
@@ -12,7 +13,7 @@ public class AdminFlowTest extends BaseTest {
         LoginPage login = new LoginPage(page);
         login.login("covacsis_admin@techprescient.com", "MqdYgv29wAq5nG8CZY58B");
         CreateAdminFlow adminFlow=new CreateAdminFlow(page, context);
-        adminFlow.createGlobalParameter();
+        //adminFlow.createGlobalParameter();
         adminFlow.createNewKPIDefinition();
 
 
